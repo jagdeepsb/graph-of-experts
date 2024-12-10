ebc9472240c336f476afd17ebb52742e4b4f53ba
Nolan Fey <nolanfey@mit.edu>
2024-12-09 22:29:42 -0500

    celeba dataset in src/data.py

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
-
+from tqdm import tqdm
 
 
 class RotatedMNISTDataset(Dataset):
@@ -74,3 +74,86 @@ class FixedSizeWrapper(Dataset):
 
     def __getitem__(self, idx):
         return self.dataset[self.samples[idx]]
+    
+class CelebADataset(Dataset):
+    def __init__(self, root: str = "./data", train: bool = True, download: bool = True, n=6000):
+        """
+        Create an extended CelebA dataset with rotated variations.
+
+        Args:
+            root (str): Root directory of dataset
+            train (bool): If True, creates dataset from training set, else test set
+            download (bool): If True, downloads the dataset if not already present
+            transform (callable, optional): Optional transform to be applied on an image
+        """
+        split = 'train' if train else 'test'
+        dataset = datasets.CelebA(root=root, split=split, download=download)
+        
+        self.path_attributes = ['Black_Hair', 'Male', 'Eyeglasses']
+        self.num_paths = int(len(self.path_attributes)**3)
+        self.attr_dict = {}
+        for i in range(2**len(self.path_attributes)):
+            attr_str = ''
+            for j, attr in enumerate(self.path_attributes):
+                if i & (1 << j):
+                    attr_str += f'{attr}; '
+                else:
+                    attr_str += f'not {attr}; '
+            self.attr_dict[i] = attr_str.strip()
+        
+        self.class_attribute = 'Smiling'
+
+        # data split
+        n = n // 2
+        d1 = [] 
+        attr1 = np.zeros((n, len(self.path_attributes)))
+        class1 = np.zeros((n,))
+        d2 = [] 
+        attr2 = np.zeros((n, len(self.path_attributes)))
+        class2 = np.zeros((n,))
+        split_attr_idx = dataset.attr_names.index(self.path_attributes[0])
+        for x, y in tqdm(dataset, desc=f'Splitting data by {self.path_attributes[0]}'):
+            if y[split_attr_idx] == 1 and len(d1) < n:
+                d1.append(x)
+                for i, attr in enumerate(self.path_attributes):
+                    attr_idx = dataset.attr_names.index(attr)
+                    attr1[len(d1)-1, i] = y[attr_idx]
+                class1[len(d1)-1] = y[dataset.attr_names.index(self.class_attribute)]
+            elif y[split_attr_idx] == 0 and len(d2) < n:
+                d2.append(x)
+                for i, attr in enumerate(self.path_attributes):
+                    attr_idx = dataset.attr_names.index(attr)
+                    attr2[len(d2)-1, i] = y[attr_idx]
+                class2[len(d2)-1] = y[dataset.attr_names.index(self.class_attribute)]
+
+            if len(d1) >= n and len(d2) >= n:
+                break
+
+        self.x = d1 + d2
+        self.attr = np.vstack((attr1, attr2))
+        self.target = np.hstack((class1, class2))
+
+    def __len__(self):
+        """
+        Total number of samples
+        """
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        """
+        Retrieve an item by index.
+
+        Returns:
+            tuple: (image, attr (int), class (int))
+        """
+        image = self.x[idx]
+        attr = 0
+        for i, attr_val in enumerate(self.attr[idx]):
+            attr += attr_val * 2**i
+        target = self.target[idx]
+
+        return (
+            image,
+            attr,
+            target,
+        )
