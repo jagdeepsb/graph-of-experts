ecc2ad12dee8a27b64bbb28457770febfe5d2aa1
Jagdeep Bhatia <jagdeep@mit.edu>
2024-12-09 22:52:17 -0500

    Implement and test vqvae on digits

--- src/clustering/kmeans.py
+++ src/clustering/kmeans.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from matplotlib import pyplot as plt
 from torch.utils.data import Dataset
 from sklearn.cluster import KMeans
+from src.vqvae.train import train_vae, get_vae
 
 class KMeansClusterer():
     def __init__(self, n_clusters, verbose=False, n_tries=10):
@@ -125,6 +126,103 @@ class KMeansImageClusterer(KMeansClusterer):
 
         # Plot images
         fig, axes = plt.subplots(self.n_clusters, n_per, figsize=(80, 80))
+        for i in range(self.n_clusters):
+            for j in range(n_per):
+                img = label_to_images[i][j]
+                img = np.transpose(img, (1, 2, 0))
+                img = (img + 1) / 2
+                axes[i, j].imshow(img)
+                axes[i, j].axis("off")
+        plt.tight_layout()
+        
+        # Save the plot
+        plt.savefig("clustered_images.png")
+        
+class KMeansVQVAEClusterer(KMeansClusterer):
+    def __init__(self, n_clusters, verbose=False, n_tries=10):
+        super().__init__(n_clusters, verbose, n_tries)
+        self.vae = None
+    
+    def fit(self, dataset: Dataset):
+        """
+        dataset: torch.utils.data.Dataset
+        Expects (image, ...) tuples
+        """
+        
+        # determine number of channels in input dim
+        input_dim = dataset[0][0].shape[0]
+        self.vae = get_vae(input_dim=input_dim)
+        
+        # train the vae
+        train_vae(self.vae, dataset)
+        
+        # get the embeddings
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        features = []
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=128, shuffle=False)
+        for el in tqdm(dataloader):
+            x = el[0]
+            x = x.to(device)
+            feat = self.vae.get_embedding(x)
+            
+            # flatten embeddings except for the batch dimension
+            feat = feat.view(feat.size(0), -1)
+            features.append(feat.cpu().detach().numpy())
+
+        features = np.concatenate(features, axis=0)
+        
+        print(f"Fitting kmeans model with {features.shape[0]} samples")
+        self.model.fit(features)
+        self._cluster_centers = self.model.cluster_centers_
+        
+    def predict(self, x: torch.Tensor) -> np.ndarray:
+        """
+        x: torch.Tensor of shape (n_samples, C, H, W)
+        returns: numpy array of shape (n_samples,)
+        """
+        
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        x = x.to(device)
+        features = self.vae.get_embedding(x)
+        features = features.view(features.size(0), -1)
+        features = features.cpu().detach().numpy()
+        ids = self.model.predict(features)
+        return ids
+        
+    def debug(self, dataset: Dataset):
+        """
+        
+        dataset: torch.utils.data.Dataset
+        Expects (image, ...) tuples
+        """
+        
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        features, raw_images = [], []
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=128, shuffle=False)
+        for el in tqdm(dataloader):
+            assert type(el) == tuple or type(el) == list
+            image = el[0]
+            assert type(image) == torch.Tensor
+            image = image.numpy()
+            raw_images.extend([im.copy() for im in image])
+            
+            x = torch.tensor(image).to(device)
+            feat = self.vae.get_embedding(x)
+            feat = feat.view(feat.size(0), -1)
+            features.append(feat.cpu().detach().numpy())
+            
+        features = np.concatenate(features, axis=0)
+        labels = self.model.predict(features)
+        
+        n_per = 20
+        label_to_images = {i: [] for i in range(self.n_clusters)}
+        for image, label in zip(raw_images, labels):
+            if len(label_to_images[label]) < n_per:
+                label_to_images[label].append(image)
+
+        # Plot images
+        fig, axes = plt.subplots(self.n_clusters, n_per, figsize=(80, 20))
         for i in range(self.n_clusters):
             for j in range(n_per):
                 img = label_to_images[i][j]
