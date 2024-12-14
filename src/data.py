667aad16ae4175b2078ae357fac8be3a65119856
Ezra Erives <erives@mit.edu>
2024-12-13 23:02:53 -0500

    Add dataset changes

--- src/data.py
+++ src/data.py
@@ -126,8 +126,11 @@ class RotatedMNISTDataset(Dataset):
 
 class CelebADataset(Dataset):
     def __init__(
-        self, root: str = "./data", train: bool = True, download: bool = True, n=3000
-    , balance_dataset=False):
+        self,
+        root: str = "./data",
+        train: bool = True,
+        download: bool = True,
+    ):
         """
         Create an extended CelebA dataset split along 3 attributes.
 
@@ -145,51 +148,21 @@ class CelebADataset(Dataset):
             ]
         )
 
-        dataset = datasets.CelebA(
+        self.dataset = datasets.CelebA(
             root=root, split=split, download=download, transform=transform
         )
 
-        self.path_attributes = ["Black_Hair", "Male", "Eyeglasses"]
-        self.num_paths = int(2**len(self.path_attributes))
-        self.attr_dict = {}
-        for i in range(2 ** len(self.path_attributes)):
-            attr_str = ""
-            for j, attr in enumerate(self.path_attributes):
-                if i & (1 << j):
-                    attr_str += f"{attr}; "
-                else:
-                    attr_str += f"not {attr}; "
-            self.attr_dict[i] = attr_str.strip()
-
-        self.class_attribute = "Smiling"
-
-        # data split
-        n = n // self.num_paths
-        d = {}
-        for i in range(self.num_paths):
-            d[i] = []
-        for x, y in tqdm(dataset, desc='Splitting data'):
-            attr = 0
-            for i, path_attr in enumerate(self.path_attributes):
-                attr_idx = dataset.attr_names.index(path_attr)
-                attr += y[attr_idx].item() * 2**i
-            target = y[dataset.attr_names.index(self.class_attribute)]
-            if len(d[attr]) < n:
-                d[attr].append((x, target))
-
-        if balance_dataset:
-            min_len = min([len(d[attr]) for attr in d])
-            for attr in d:
-                d[attr] = d[attr][:min_len]
-
-        self.d = d
-        self.dataset_lengths = [len(d[attr]) for attr in d]
+        self.path_attr_indices = [
+            self.dataset.attr_names.index(attr)
+            for attr in ["Black_Hair", "Male", "Eyeglasses"]
+        ]
+        self.target_attr_index = self.dataset.attr_names.index("Smiling")
 
     def __len__(self):
         """
         Total number of samples
         """
-        return sum(self.dataset_lengths)
+        return len(self.dataset)
 
     def __getitem__(self, idx):
         """
@@ -198,21 +171,56 @@ class CelebADataset(Dataset):
         Returns:
             tuple: (image, attr (int), class (int))
         """
-        for i, length in enumerate(self.dataset_lengths):
-            if idx < length:
-                image, target = self.d[i][idx]
-                attr = i
-                break
-            idx -= length
+        # Get the original CelebA dataset item
+        image, attrs = self.dataset[idx]
 
-        # attr to int64
-        attr = np.array(attr, dtype=np.int64)
+        # Extract only the selected attributes
+        path_attrs = attrs[self.path_attr_indices]
+        target_attr = attrs[self.target_attr_index]
 
-        # target to int64
-        target = np.array(target, dtype=np.int64)
+        # Encode path_attrs into integer representing binary representation
+        path_key = sum([2**i for i, attr in enumerate(path_attrs) if attr == 1])
+
+        return image, path_key, target_attr
+
+    @classmethod
+    def get_celeba_loaders(
+        cls, downsample_factor: int = 20
+    ) -> Tuple[Dataset, Dataset, Dataset, DataLoader, DataLoader, DataLoader]:
+        # Initialize dataset
+        dataset = cls()
+
+        # Assuming `dataset` is your PyTorch Dataset
+        dataset_size = len(dataset)
+        train_size = int(0.7 * dataset_size)
+        val_size = int(0.2 * dataset_size)
+        test_size = dataset_size - train_size - val_size
+
+        train_dataset, val_dataset, test_dataset = random_split(
+            dataset,
+            [train_size, val_size, test_size],
+            generator=torch.Generator().manual_seed(40),
+        )
+
+        train_dataset = FixedSizeWrapper(
+            dataset=train_dataset, size=train_size // downsample_factor
+        )
+        val_dataset = FixedSizeWrapper(
+            dataset=val_dataset, size=val_size // downsample_factor
+        )
+        test_dataset = FixedSizeWrapper(
+            dataset=test_dataset, size=test_size // downsample_factor
+        )
+
+        train_loader = DataLoader(train_dataset, batch_size=32, shuffle=False)
+        val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
         return (
-            image, 
-            attr,
-            target,
+            train_dataset,
+            val_dataset,
+            test_dataset,
+            train_loader,
+            val_loader,
+            test_loader,
         )
